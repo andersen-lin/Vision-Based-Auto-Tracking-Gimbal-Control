826249d0b5e434db06833ed383759936ffb1296e
Andersen Lin <andersen1997@126.com>
2020-05-15 12:46:57 -0500

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -1,4 +1,5 @@
 /* Author Name: Andersen Lin
+ * Email: andersen1997801@gmail.com
  * Discription: this is a demo code for that works with Nuitrack SDK and ROS
  * it rotates a gimbal based on the inputs from joint positions and collaborates with a robot to trace a human body 
  * To Compile: g++ -std=c++11 Sender.cpp -o Sender -lserial
